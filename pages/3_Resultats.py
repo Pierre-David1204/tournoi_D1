47886b536c1892d9dc82c54cb7250dfe95785378
Pierre-David1204 <pierredavid.dapoz@condorcet.be>
2026-03-17 10:05:13 +0100

    Create 3_Resultats.py

---
+++ pages/3_Resultats.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+from supabase import create_client
+
+url = "https://yzupjrzhqmojefurpmrx.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dXBqcnpocW1vamVmdXJwbXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTY0ODcsImV4cCI6MjA4ODk5MjQ4N30.4qYKmPfDagkicbC31aob3egY2msh7mzuk7ECRJ2-M1A"
+
+
+supabase = create_client(url, key)
+
+st.title("📋 Résultats D1")
+
+equipes_data = supabase.table("d1_equipes").select("*").execute()
+
+equipes = {e["id"]:e["nom"] for e in equipes_data.data}
+
+data = supabase.table("d1_matchs") \
+    .select("*") \
+    .eq("termine",True) \
+    .execute()
+
+df = pd.DataFrame(data.data)
+
+df["Equipe1"] = df["equipe1"].map(equipes)
+df["Equipe2"] = df["equipe2"].map(equipes)
+
+for _,m in df.iterrows():
+
+    st.write(
+        f"{m['Equipe1']} {m['score1']} - {m['score2']} {m['Equipe2']}"
+    )
